966e738be451750f40f6623bef7af5f410e2b8a4
Peter Friese <peter@peterfriese.de>
2022-02-08 15:28:52 +0100

    🥅 Update error enum

--- Networking/SwiftUICombineNetworking/SwiftUICombineNetworking/Features/Authentication/Services/AuthenticationService.swift
+++ Networking/SwiftUICombineNetworking/SwiftUICombineNetworking/Features/Authentication/Services/AuthenticationService.swift
@@ -14,75 +14,56 @@ struct UserNameAvailableMessage: Codable {
   var userName: String
 }
 
-enum NetworkError: Error {
+struct APIErrorMessage: Decodable {
+  var error: Bool
+  var reason: String
+}
+
+enum APIError: LocalizedError {
+  /// Invalid request, e.g. invalid URL
   case invalidRequestError(String)
+
+  /// Indicates an error on the transport layer, e.g. not being able to connect to the server
   case transportError(Error)
-  case serverError(statusCode: Int)
-  case noData
+  
+  /// Received an invalid response, e.g. non-HTTP result
+  case invalidResponse
+  
+  /// Server-side validation error
+  case validationError(String)
+  
+  /// General server-side error. If `retryAfter` is set, the client can send the same request after the given time.
+  case serverError(statusCode: Int, reason: String? = nil, retryAfter: String? = nil)
+  
   case decodingError(Error)
   case encodingError(Error)
+  
+  case noData
+  
+  var errorDescription: String? {
+    switch self {
+    case .invalidRequestError(let message):
+      return "Invalid request: \(message)"
+    case .transportError(let error):
+      return "Transport error: \(error)"
+    case .invalidResponse:
+      return "Invalid response"
+    case .validationError(let reason):
+      return "Validation Error: \(reason)"
+    case .serverError(let statusCode, let reason, let retryAfter):
+      return "Server error with code \(statusCode), reason: \(reason ?? "no reason given"), retry after: \(retryAfter ?? "no retry after provided")"
+    case .decodingError(let error):
+      return "Decoding error: \(error)"
+    case .encodingError(let error):
+      return "Encoding error: \(error)"
+    case .noData:
+      return "No data"
+    }
+  }
 }
 
 struct AuthenticationService {
-  
-  /// 1: Fetching data using URLSession *without* using Combine
-  func checkUserNameAvailableOldSchool(userName: String, completion: @escaping (Result<Bool, NetworkError>) -> Void) {
-    guard let url = URL(string: "http://127.0.0.1:8080/isUserNameAvailable?userName=\(userName)") else  {
-      completion(.failure(.invalidRequestError("URL invalid")))
-      return
-    }
-    
-    let task = URLSession.shared.dataTask(with: url) { data, response, error in
-      if let error = error {
-        completion(.failure(.transportError(error)))
-        return
-      }
-      
-      if let response = response as? HTTPURLResponse, !(200...299).contains(response.statusCode) {
-        completion(.failure(.serverError(statusCode: response.statusCode)))
-        return
-      }
-      
-      guard let data = data else {
-        completion(.failure(.noData))
-        return
-      }
-      
-      do {
-        let decoder = JSONDecoder()
-        let userAvailableMessage = try decoder.decode(UserNameAvailableMessage.self, from: data)
-        completion(.success(userAvailableMessage.isAvailable))
-      }
-      catch {
-        completion(.failure(.decodingError(error)))
-      }
-    }
-    
-    task.resume()
-  }
-  
-  /// 2: The same code, refactored to make use of Combine to fetch data. Mapping data are not optimised at this stage.
-  func checkUserNameAvailableNaive(userName: String) -> AnyPublisher<Bool, Never> {
-    guard let url = URL(string: "http://127.0.0.1:8080/isUserNameAvailable?userName=\(userName)") else {
-      return Just(false).eraseToAnyPublisher()
-    }
-    
-    return URLSession.shared.dataTaskPublisher(for: url)
-      .map { data, response in
-        do {
-          let decoder = JSONDecoder()
-          let userAvailableMessage = try decoder.decode(UserNameAvailableMessage.self, from: data)
-          return userAvailableMessage.isAvailable
-        }
-        catch {
-          return false
-        }
-      }
-      .replaceError(with: false)
-      .eraseToAnyPublisher()
-  }
-  
-  /// 3: The same code, making full use of Combine's capabilities for mapping data
+
   func checkUserNameAvailable(userName: String) -> AnyPublisher<Bool, Never> {
     guard let url = URL(string: "http://127.0.0.1:8080/isUserNameAvailable?userName=\(userName)") else {
       return Just(false).eraseToAnyPublisher()
